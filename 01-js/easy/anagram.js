1b9edd697a6a687a6551ba2a15a5b513592cebfa
codeadarsh101 <122601329+codeadarsh101@users.noreply.github.com>
2023-06-28 20:10:19 +0530

    Update anagram.js
    Here is the new update of anagram problem!

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,24 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function sort(str){
+    var array=str.split("");
+    array=array.sort();
+         var sortedstring=array.join("");
+         return sortedstring;
 
 }
 
-module.exports = isAnagram;
+function isAnagram(str1,str2){
+  if(sort(str1)==sort(str2)){
+    return true;
+  }  
+  else{
+    return false;
+  }
+}
+var ans=isAnagram("asekl","sear");
+console.log(ans);
+  
+
+
